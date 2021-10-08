0544f5b6cd081a3e84ba896777d1f5e2e1e4c0f2
Seunghwanly <seunghwanly@gmail.com>
2021-10-08 11:31:19 +0900

    android get result after file upload
    - asynchronous way to get result
    - copying real file to cachdir
    - added multipart request interface

--- android/app/src/main/java/com/example/des3776/HypertensionAPI.kt
+++ android/app/src/main/java/com/example/des3776/HypertensionAPI.kt
@@ -1,13 +1,19 @@
 package com.example.des3776
 
 import com.google.gson.JsonObject
+import okhttp3.MultipartBody
 import retrofit2.Call
-import retrofit2.http.GET
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface HyperTensionAPI {
     @GET("hypertensions")
     fun getEvaluation(
         @Query("index") index: Int
+    ): Call<JsonObject>
+
+    @Multipart
+    @POST("hypertensions")
+    fun getResultFromFile(
+        @Part body: MultipartBody.Part,
     ): Call<JsonObject>
 }
--- android/app/src/main/java/com/example/des3776/MainActivity.kt
+++ android/app/src/main/java/com/example/des3776/MainActivity.kt
@@ -1,32 +1,49 @@
 package com.example.des3776
 
 import android.annotation.SuppressLint
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.view.View
-import android.widget.*
+import android.widget.Button
+import androidx.activity.result.ActivityResultCallback
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.des3776.databinding.ActivityMainBinding
 import com.google.gson.JsonObject
-import retrofit2.*
+import okhttp3.MediaType
+import okhttp3.MultipartBody
+import okhttp3.RequestBody
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.io.File
+import java.io.FileInputStream
+import java.io.FileOutputStream
+
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
     private lateinit var viewmodel: HypertensionViewModel
-    private var selectedIndex: Int = 1
+    private var selectedFileURI: Uri? = null
+    private lateinit var intentLauncher: ActivityResultLauncher<Intent>
 
+    //    private lateinit var intentLauncher: ActivityResultLauncher<String>
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         // Data binding
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
-        viewmodel = ViewModelProviders.of(this).get(HypertensionViewModel::class.java)
+        viewmodel = ViewModelProvider(this).get(HypertensionViewModel::class.java)
+
         binding.activity = this@MainActivity
         setRecyclerView()
 
@@ -40,26 +57,20 @@ class MainActivity : AppCompatActivity() {
             }
         viewmodel.observableData.observe(this, dataObserver)
 
-        val reqText: TextView = findViewById(R.id.desc_text)
-        val radioGroup: RadioGroup = findViewById(R.id.radio_group)
-        val radio1: RadioButton = findViewById(R.id.radio_1)
-        val radio2: RadioButton = findViewById(R.id.radio_2)
-        val radio3: RadioButton = findViewById(R.id.radio_3)
-        val reqBtn: Button = findViewById(R.id.req_btn)
-
-        // set attributes
-        radioGroup.setOnCheckedChangeListener { radioGroup, index ->
-            when (index) {
-                R.id.radio_1 -> selectedIndex = 1
-                R.id.radio_2 -> selectedIndex = 2
-                R.id.radio_3 -> selectedIndex = 3
+        intentLauncher = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult(),
+//            ActivityResultContracts.GetContent(),
+            ActivityResultCallback { res ->
+//                selectedFileURI = res
+                if (res.resultCode == RESULT_OK) {
+                    selectedFileURI = res.data?.data
+                    Log.d("onActivityResult", selectedFileURI!!.toString())
+                }
             }
-            reqText.text = "You have selected " + selectedIndex.toString()
-        }
-        radio1.text = "First Test Case"
-        radio2.text = "Second Test Case"
-        radio3.text = "Third Test Case"
-        reqBtn.text = "Send"
+        )
+
+        val reqBtn: Button = findViewById(R.id.req_btn)
+        reqBtn.text = "Select File and Send"
     }
 
     private fun setRecyclerView() {
@@ -70,43 +81,82 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun getEvaluationOfSelectedTestCase(view: View) {
+        /// select file first
+        val selectFileIntent = Intent().setType("*/*").setAction(Intent.ACTION_GET_CONTENT)
+        selectFileIntent.addCategory(Intent.CATEGORY_OPENABLE)
+//        selectFileIntent.type = "text/comma-separated-values"
+        selectFileIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        // execute
+        intentLauncher.launch(selectFileIntent)
 
-        val baseURL = "http://10.0.2.2:5000/"
-        // use Retrofit to create request
-        val retrofit =
-            Retrofit.Builder().baseUrl(baseURL).addConverterFactory(GsonConverterFactory.create())
-                .build()
-        val api = retrofit.create(HyperTensionAPI::class.java)
-        val callGetEvaluation = api.getEvaluation(selectedIndex)
+        if (selectedFileURI != null) {
+            // convert to inputStream
+            val inputStream = contentResolver.openInputStream(selectedFileURI!!)!!
+            val newlyWrittenFile =
+                File(cacheDir.absolutePath + "/" + selectedFileURI!!.port)
 
-        callGetEvaluation.enqueue(object : Callback<JsonObject> {
-            override fun onResponse(
-                call: Call<JsonObject>,
-                response: Response<JsonObject>
-            ) {
-                val results = response.body()?.getAsJsonArray("result")
+            // write new file with outputStream
+            val outputStream = FileOutputStream(newlyWrittenFile)
+            val buf = ByteArray(1024)
+            var len: Int
+            while (true) {
+                len = inputStream.read(buf, 0, 1024)
+                if (len > 0) {
+                    outputStream.write(buf, 0, len)
+                } else break
+            }
 
-                if (results != null) {
-                    val parsedHypertensions: MutableList<Hypertension> = mutableListOf()
-                    for (res in results) {
-                        val resObject = res.asJsonObject
-                        val name = resObject.get("name").asString
-                        val count = resObject.get("cnt").asInt
-                        val maxP = resObject.get("max_p").asDouble
-                        val minP = resObject.get("min_p").asDouble
-                        // save to list
-                        val item = Hypertension(name, count, maxP, minP)
-                        parsedHypertensions.add(item)
+            outputStream.close()
+
+            Log.d("fileNEW", "wrote everything")
+
+            val requestFile = RequestBody.create(
+                MediaType.parse(contentResolver.getType(selectedFileURI!!)!!),
+                newlyWrittenFile
+            )
+            val requestBody =
+                MultipartBody.Part.createFormData("file", newlyWrittenFile.name, requestFile)
+
+            // execute the request
+            // use Retrofit to create request
+            val baseURL = "http://10.0.2.2:3000/"
+            val retrofit =
+                Retrofit.Builder().baseUrl(baseURL)
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build()
+            val api = retrofit.create(HyperTensionAPI::class.java)
+            val call = api.getResultFromFile(requestBody)
+
+            /// use in asychronous way
+            call.enqueue(object : Callback<JsonObject> {
+                override fun onResponse(
+                    call: Call<JsonObject>,
+                    response: Response<JsonObject>
+                ) {
+                    Log.d("onCall", call.isExecuted.toString() + call.toString())
+                    val results = response.body()?.getAsJsonArray("result")
+
+                    if (results != null) {
+                        val parsedHypertensions: MutableList<Hypertension> = mutableListOf()
+                        for (res in results) {
+                            val resObject = res.asJsonObject
+                            val name = resObject.get("name").asString
+                            val count = resObject.get("cnt").asInt
+                            val maxP = resObject.get("max_p").asDouble
+                            val minP = resObject.get("min_p").asDouble
+                            // save to list
+                            val item = Hypertension(name, count, maxP, minP)
+                            parsedHypertensions.add(item)
+                        }
+                        Log.d("getEvaluationOfSelectedTestCase", parsedHypertensions.toString())
+                        viewmodel.observableData.postValue(parsedHypertensions as ArrayList<Hypertension>?)
                     }
-                    Log.d("getEvaluationOfSelectedTestCase", parsedHypertensions.toString())
-                    viewmodel.observableData.postValue(parsedHypertensions as ArrayList<Hypertension>?)
                 }
-                Log.d("onRequest", "success ${results.toString()}")
-            }
 
-            override fun onFailure(call: Call<JsonObject>, t: Throwable) {
-                Log.d("onRequest", "failed $t")
-            }
-        })
+                override fun onFailure(call: Call<JsonObject>, t: Throwable) {
+                    Log.d("onRequest", "failed $t")
+                }
+            })
+        }
     }
 }
